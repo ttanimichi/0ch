3b83999903576722180c9fd50d866d22325a0195
Tsukuru Tanimichi <info+git@ttanimichi.com>
2017-08-18 13:18:52 +0900

    スレッドの一覧を表示できる

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'slim'
 
+require_relative 'models/topic'
+
 get '/' do
+  @topics = Topic.all
   slim :index
 end
--- models/topic.rb
+++ models/topic.rb
@@ -1,4 +1,13 @@
 require_relative 'application_entity'
 
 class Topic < ApplicationEntity
+  attr_reader :title, :id, :created_at, :updated_at
+
+  def initialize(id: nil, title:, created_at: nil, updated_at: nil)
+    @id, @title, @created_at, @updated_at = id, title, created_at, updated_at
+  end
+
+  def self.all
+    DB.query('SELECT * FROM topics ORDER BY updated_at DESC').map { |hash| new(hash) }
+  end
 end

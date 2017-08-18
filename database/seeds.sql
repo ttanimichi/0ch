21caf3f50332b8f49bfc0c9d17cea7778c0501f9
Tsukuru Tanimichi <info+git@ttanimichi.com>
2017-08-18 12:46:58 +0900

    topics テーブルの seed データを作成

---
+++ database/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO topics (title, created_at, updated_at) VALUES ('スレタイ1', NOW(), NOW());
+INSERT INTO topics (title, created_at, updated_at) VALUES ('スレタイ2', NOW(), NOW());
--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 
 mysql -u root < ./database/create_database.sql
 mysql -u root 0ch < ./database/create_topics.sql
+mysql -u root 0ch < ./database/seeds.sql

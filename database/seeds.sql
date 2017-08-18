eb023ebf890dbcf3519673f5928cdb4b2c9e9222
Tsukuru Tanimichi <info+git@ttanimichi.com>
2017-08-18 20:57:35 +0900

    posts テーブルを作成

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,2 +1,10 @@
 INSERT INTO topics (title, created_at, updated_at) VALUES ('スレタイ1', NOW(), NOW());
 INSERT INTO topics (title, created_at, updated_at) VALUES ('スレタイ2', NOW(), NOW());
+
+INSERT INTO posts (name, email, body, topic_id, created_at) VALUES ('名無し', '', 'レス1', 1, NOW());
+INSERT INTO posts (name, email, body, topic_id, created_at) VALUES ('名無し', '', 'レス2', 1, NOW());
+INSERT INTO posts (name, email, body, topic_id, created_at) VALUES ('名無し', '', 'レス3', 1, NOW());
+
+INSERT INTO posts (name, email, body, topic_id, created_at) VALUES ('名無し', '', 'レス4', 2, NOW());
+INSERT INTO posts (name, email, body, topic_id, created_at) VALUES ('名無し', '', 'レス5', 2, NOW());
+INSERT INTO posts (name, email, body, topic_id, created_at) VALUES ('名無し', '', 'レス6', 2, NOW());
--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 
 mysql -u root < ./database/create_database.sql
 mysql -u root 0ch < ./database/create_topics.sql
+mysql -u root 0ch < ./database/create_posts.sql
 mysql -u root 0ch < ./database/seeds.sql
